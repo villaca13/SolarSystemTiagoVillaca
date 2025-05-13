06b17e39aff9d72ee973bbe60bef7c819ca4ed20
Tiago Villaca <60266341+villaca13@users.noreply.github.com>
2025-05-13 23:07:53 +0100

    menu implementation

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -20,33 +20,149 @@ public class Driver {
 
     //TODO - load all data once the serializers are set up
     runMainMenu();
-}
+    }
+
+
 //TODO - construct menus
+    //---------------------
+    //  MAIN Menu Items
+    //---------------------
 
     private int mainMenu() {
 
         //TODO write menu that user will see
-        return ScannerInput.readNextInt("==>> ");
+        int option = ScannerInput.readNextInt("""
+                ------------------------------------------------------------------
+                |                        Space Place                             |
+                ------------------------------------------------------------------
+                |   1) Planets CRUD MENU                                         | 
+                |                                                                |
+                |   2) Reports MENU                                              |                                          | 
+                ------------------------------------------------------------------
+                |   3) Search Planets                                            |
+                |                                                                |
+                |   4) Sort Planets                                              |
+                ------------------------------------------------------------------            
+                |   10) Load All                                                 |
+                |   11) Save All                                                 |
+                ------------------------------------------------------------------
+                |   0)  Exit                                                     |  
+                ------------------------------------------------------------------
+                ==>>  """);
+        return option;
     }
-
 
     private void runMainMenu() {
         int option = mainMenu();
         //TODO - write code to call appropiate method based on value in option
+
+        while (option != 0) {
+
+            switch (option) {
+                case 1 -> runPlanetsMenu();
+                case 2 -> runReportsMenu();
+                case 3 -> exitApp();
+                case 4 -> exitApp();
+                case 10 -> exitApp();
+                case 11 -> exitApp();
+
+                default -> System.out.println("Invalid option entered: " + option);
+            }
+
+            //pause the program so that the user can read what we just printed to the terminal window
+            ScannerInput.readNextLine("\nPress enter key to continue...");
+
+            //display the main menu again
+            option = mainMenu();
+        }
         exitApp();
     }
+    //---------------------
+    //  PLANETS Menu Items
+    //---------------------
+    private int planetsMenu() {
 
-    private void exitApp() {
+        //TODO write menu that user will see
+        int option = ScannerInput.readNextInt("""
+                ------------------------------------------------------------------
+                |                    Planet Object Menu                          |
+                ------------------------------------------------------------------
+                |   1) Add a Planet Object                                       | 
+                |   2) Delete a Planet Object                                    |                                          | 
+                |   3) List all Planet Object                                    |
+                |   4) Update a Planet Object                                    |
+                |   0) Return to Main Menu                                       |  
+                ------------------------------------------------------------------
+                ==>>  """);
+        return option;
+    }
 
-        System.out.println("Exiting....");
-        System.exit(0);
+    private void runPlanetsMenu() {
+        int option = planetsMenu();
+        //TODO - write code to call appropiate method based on value in option
+
+        while (option != 0) {
+
+            switch (option) {
+                case 1 -> exitApp();
+                case 2 -> exitApp();
+                case 3 -> exitApp();
+                case 4 -> exitApp();
+
+                default -> System.out.println("Invalid option entered: " + option);
+            }
+
+            //pause the program so that the user can read what we just printed to the terminal window
+            ScannerInput.readNextLine("\nPress enter key to continue...");
+
+            //display the main menu again
+            option = planetsMenu();
+        }
+        runMainMenu();
+    }
+
+    //---------------------
+    //  Reports Menu Items
+    //---------------------
+    private int reportsMenu() {
+
+        //TODO write menu that user will see
+        int option = ScannerInput.readNextInt("""
+                ------------------------------------------------------------------
+                |                      Reports Menu                              |
+                ------------------------------------------------------------------
+                |   1) Planets Overview                                          | 
+                |   0) Return to Main Menu                                       |  
+                ------------------------------------------------------------------
+                ==>>  """);
+        return option;
+    }
+
+    private void runReportsMenu() {
+        int option = reportsMenu();
+        //TODO - write code to call appropiate method based on value in option
+
+        while (option != 0) {
+
+            switch (option) {
+                case 1 -> exitApp();
+
+                default -> System.out.println("Invalid option entered: " + option);
+            }
+
+            //pause the program so that the user can read what we just printed to the terminal window
+            ScannerInput.readNextLine("\nPress enter key to continue...");
+
+            //display the main menu again
+            option = reportsMenu();
+        }
+        runMainMenu();
     }
 
 
+//TODO update methods counting methods
 
 
-
-    //todo update methods counting methods
 
 
     //---------------------
@@ -54,17 +170,29 @@ public class Driver {
     //---------------------
 
 //TODO - write all the methods that are called from your menu
+
+
+
+
     //---------------------
     //  Search/Sort
     //---------------------
 // TODO search by different criteria i.e. look at the list methods and give options based on that.
 // TODO sort  (and give a list of options - not a recurring menu thou)
+
+
+
+
     //---------------------
     //  Helper Methods
     //---------------------
 
 //TODO- write any helper methods that are required
+    private void exitApp() {
 
+        System.out.println("Exiting....");
+        System.exit(0);
+    }
 
 }
 
